9f0c9d58ee6b6cd6a6b8d765dbb9a8cbdb1de080
26messias26 <messias26severino@gmail.com>
2023-07-06 08:28:43 -0300

    Controller do usuario e perfil

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,4 +1,7 @@
 class HomeController < ApplicationController
   def index
+    unless user_signed_in?
+      @user = User.new
+    end
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,25 @@
 Rails.application.routes.draw do
-  devise_for :users
+ 
+  devise_for :users, controllers:{
+    sessions: 'users',
+    passwords: 'users/passwords',
+    registrations: 'users/registrations',
+    confirmations: 'users/confirmations'
+  }
   
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
+
+  # Abre uma pagina com o email enviado
+  if Rails.env.development?
+    mount LetterOpenerWeb::Engine, at: "/letter_opener"
+  end
+
+  resources :users, only: [:show]
+
+  resources :livros
+
   root "home#index"
+
 end
